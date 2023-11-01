722a5aaafcafd2daad004578fcf18ee65db4cf55
owner <owner@DESKTOP-4O1GNO6>
2023-11-01 08:35:17 +0330

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using System;
+
+
+            public class GFG
+        {
+            public static int Fib(int n)
+            {
+                if (n <= 1)
+                {
+                    return n;
+                }
+                else
+                {
+                    return Fib(n - 1) + Fib(n - 2);
+                }
+            }
+
+            // driver code 
+            public static void Main(string[] args)
+            {
+                int n = 9;
+                Console.Write(Fib(n));
+            }
+        }
+
+        // مهدی کامرانی نژاد
+    }
+}
+}
